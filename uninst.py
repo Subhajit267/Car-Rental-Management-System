65a95eb7ad74d851d62f86fd8f414acd66c9e47b
Subhajit Halder <85571191+Subhajit267@users.noreply.github.com>
2025-09-28 01:31:11 +0530

    Update uninst.py

--- uninst.py
+++ uninst.py
@@ -1,4 +1,10 @@
+'''
+    This is unistallation script for uninstalling CRMS
+    Author: SUBHAJIT HALDER 
+       DATE: 12/01/2024
+'''
 import tempfile,os,sys
 os.system(r"cd C:\temp")
 os.system(r"copy .\\unst.exe c:\\windows\temp\\unst.exe")
 os.startfile(r"c:\windows\temp\unst.exe")
+
